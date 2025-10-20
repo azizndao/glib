53bece7c18f752aeb2b171d491ba07ed4fe6aa69
Abdou Aziz NDAO <abdouazizndao.dev@gmail.com>
2025-10-20 02:04:27 +0000

    refactor: Simplify FirstOrDefault function by removing unnecessary else statement

--- util/default.go
+++ util/default.go
@@ -3,7 +3,6 @@ package util
 func FirstOrDefault[T any](values []T, defaultValue func() T) T {
 	if len(values) > 0 {
 		return values[0]
-	} else {
-		return defaultValue()
 	}
+	return defaultValue()
 }
